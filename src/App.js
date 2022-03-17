b92395262a235f34762e084bf4782531ac2874c2
prudhvinag06 <prudhvinag637@gmail.com>
2022-03-17 09:28:03 +0530

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.css';
 import FirstComponent from './components/learning-examples/FirstComponent';
 import Counter from './components/counter/Counter';
 import TodoApp from './components/todo/TodoApp'
+
 
 class App extends Component {
   render() {
--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 import withNavigation from "./WithNavigation";
 import withParams from "./WithParams.jsx";
 
@@ -7,31 +7,43 @@ import withParams from "./WithParams.jsx";
 export default class TodoApp extends Component{
     render(){
         const LoginComponentWithNavigation = withNavigation(LoginComponent);
-        // const WelcomeComponentWithParams = withParams(WelcomeComponent);
+        const WelcomeComponentWithParams = withParams(WelcomeComponent);
 
         return(
             <div className="TodoApp">
                 <Router>
+                    <HeaderComponent />
                     <Routes>
                         <Route path="/" element={<LoginComponentWithNavigation />} />
                         <Route path="/login" element={<LoginComponentWithNavigation />} />
-                        {/* <Route path="/welcome/:name" element={<WelcomeComponentWithParams />} /> */}
+                        {/* <Route path="/welcome" element={<WelcomeComponent />} /> */}
+                        <Route path="/welcome/:name" element={<WelcomeComponentWithParams />} />
                         <Route path="/todos" element={<ListTodosComponent />} />
                         <Route path="*" element={<ErrorComponent />} />
                     </Routes>
+                    <FooterComponent/>
                 </Router>
             </div>
         )
     }
 }
+
+class WelcomeComponent extends Component{
+    render() {
+        return (
+            <div>Welcome {this.props.params.name}. You can manage your todos <Link to = "/todos">here</Link></div>
+        )        
+    }
+}
+
 class ListTodosComponent extends Component {
     constructor(props){
         super(props);
         this.state = {
             todos : [
-                {id : 1, description : 'Learn React'},
-                {id : 2, description : 'Learn Java'},
-                {id : 3, description : 'Learn Sql'}
+                {id : 1, description : 'Learn React', done : false, targetDate : new Date()},
+                {id : 2, description : 'Learn Java', done : false, targetDate : new Date()},
+                {id : 3, description : 'Learn Sql', done : false, targetDate : new Date()}
             ]
         }
     }
@@ -45,6 +57,8 @@ class ListTodosComponent extends Component {
                         <tr>
                             <th>id</th>
                             <th>description</th>
+                            <th>status</th>
+                            <th>targetDate</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -55,6 +69,8 @@ class ListTodosComponent extends Component {
                                 <tr>
                                 <td>{todo.id}</td>
                                 <td>{todo.description}</td>
+                                <td>{todo.done.toString()}</td>
+                                <td>{todo.targetDate.toString()}</td>
                                 </tr>
                             )
                            
@@ -64,6 +80,35 @@ class ListTodosComponent extends Component {
                 </table>
             </div>
         )        
+    }
+}
+class HeaderComponent extends Component{
+    render(){
+        return (
+            <header>
+                <nav className = "navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href = "http://www.leetcode.com" className="navbar-brand">Prudhvi</a></div>
+                    <ul className = "navbar-nav">
+                        <li ><Link className = "navbar-nav" to = "/welcome/prudhvi">Home</Link></li>
+                        <li ><Link className = "navbar-nav"to = "/todos">Todos</Link> </li>
+                    </ul>
+                    <ul className = "navbar-nav navbar-collapse justify-content-end">
+                        <li ><Link className = "navbar-nav" to = "/login">Login</Link> </li>
+                        <li ><Link to = "/logout">Logout</Link> </li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
+class FooterComponent extends Component{
+    render(){
+        return (
+            <div>
+                <hr/>Footer
+            </div>
+        )
     }
 }
 class LoginComponent extends Component{
